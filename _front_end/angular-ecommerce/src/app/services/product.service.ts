eea25d0559757303ad447e2c0448df85904d75d8
Romulo <romulojusto@gmail.com>
2023-02-16 13:34:47 -0500

    Added initial front end component

---
+++ _front_end/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// Services can be injected into other classes/components/ "root" can be injected globally
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  // injects httpClient
+  constructor(private httpClient: HttpClient){}
+  
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      // maps the json data and returns an array
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+// Unwraps JSON from Spring Data Rest _embedded entry.
+interface GetResponse{
+  _embedded:{
+    products: Product[];
+  }
+}
